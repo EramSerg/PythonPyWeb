ff8c048c7bb9fa17c72785b2e14bc7504609d269
eramsu <eramserg@gmail.com>
2024-04-16 20:47:59 +0300

    commit lab 4

--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'apps.app',
     'apps.db_train',
     'apps.api',
